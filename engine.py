eb2ad51d58722f54eff12669681d03c331920b0b
dkrivaa <dannykrivaa@hotmail.com>
2023-07-20 20:44:16 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -4,9 +4,11 @@ import streamlit as st
 def home_title():
     st.set_page_config(page_title='Strategy',
                        layout='wide',
-                       page_icon=':Globe:')
+                       page_icon='https://www.flaticon.com/free-icon/worldwide_7368309?term=globe&page=1&position=1&origin=search&related_id=7368309')
     st.markdown(f'<span style="color: #18448c; '
                 f'font-size: 32px"><b>Strategic Planning</b></span>'
                 , unsafe_allow_html=True)
-    # st.write('Failing to plan is planning to fail')
+    st.markdown(f'<span style="color: #18448c; '
+                f'font-size: 18px"><b>Strategic Planning</b></span>'
+                , unsafe_allow_html=True)
     st.markdown('___')
