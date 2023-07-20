63c7ed9e9bb7b9b2885d692032f4735ff8a211ac
dkrivaa <dannykrivaa@hotmail.com>
2023-07-20 21:42:06 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 
 def home_title():
-    st.set_page_config(page_title='Strategy',
+    st.set_page_config(page_title='StraApp',
                        layout='wide',)
     st.markdown(f'<span style="color: #18448c; '
-                f'font-size: 32px"><b>Strategic Planning</b></span>'
+                f'font-size: 32px"><b>StraApp - Strategic Planning</b></span>'
                 , unsafe_allow_html=True)
     st.markdown(f'<span style="color: #ed7011; '
                 f'font-size: 18px"><b>'
