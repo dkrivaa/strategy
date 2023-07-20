b3df191196046091c185059257c2e83d000af9e2
dkrivaa <dannykrivaa@hotmail.com>
2023-07-20 21:28:18 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -21,7 +21,7 @@ def explanation():
                  'attempt to prepare for and embrace the opportunities and challenges '
                  'of the future. This significant process enables stakeholders '
                  'to define strategic goals, identify possible paths and facilitate '
-                 'a proactive decision-making process.\n This App is intended '
-                 'to assist any organization, public or private, '
+                 'a proactive decision-making process.\n'
+                 'This App is intended to assist any organization, public or private, '
                  'to perform a basic strategic process by identifying the main '
                  'areas that warrant attention and explicit strategic decisions')
