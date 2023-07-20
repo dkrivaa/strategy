74afd784230f1b2cce118e10763e5007678d5613
dkrivaa <dannykrivaa@hotmail.com>
2023-07-20 20:45:13 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -3,8 +3,7 @@ import streamlit as st
 
 def home_title():
     st.set_page_config(page_title='Strategy',
-                       layout='wide',
-                       page_icon='https://www.flaticon.com/free-icon/worldwide_7368309?term=globe&page=1&position=1&origin=search&related_id=7368309')
+                       layout='wide',)
     st.markdown(f'<span style="color: #18448c; '
                 f'font-size: 32px"><b>Strategic Planning</b></span>'
                 , unsafe_allow_html=True)
