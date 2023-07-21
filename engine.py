e81af9a888aa3239cd2c2570e9489ea8661fce4c
dkrivaa <dannykrivaa@hotmail.com>
2023-07-21 13:18:49 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -26,7 +26,8 @@ def data_input():
 
         e_var1 = st.text_input('#1.')
         e_var1_sig = st.select_slider('Significance to organization',
-                                      ['low', 'high'])
+                                      ['low', 'average', 'above average', 'high'])
+        e_var1_prob = st.slider('probability', range(0, 100, 10))
 
         st.form_submit_button('continue')
 
