047ec294f65d10b32822dd8f5f9302e38d3d94f8
dkrivaa <dannykrivaa@hotmail.com>
2023-07-21 16:46:31 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -69,17 +69,16 @@ def data_input():
         st.form_submit_button('**Continue**')
 
 
-    st.write(e_var1)
-    # e_var = []
-    # e_var_sig = []
-    # e_var_prob = []
-    # for i in range(0, 5):
-    #     e_var.append('e_var' + str(i))
-    #     e_var_sig.append(('e_var' + str(i) + '_sig'))
-    #     e_var_prob.append(('e_var' + str(i) + '_prob'))
-    # dfe = pd.DataFrame({'e_var': e_var, 'e_var_sig': e_var_sig, 'e_var_prob': e_var_prob})
-    #
-    # st.write(dfe)
+    e_var = []
+    e_var_sig = []
+    e_var_prob = []
+    for i in range(0, 5):
+        e_var.append('e_var' + str(i))
+        e_var_sig.append(('e_var' + str(i) + '_sig'))
+        e_var_prob.append(('e_var' + str(i) + '_prob'))
+    dfe = pd.DataFrame({'e_var': e_var, 'e_var_sig': e_var_sig, 'e_var_prob': e_var_prob})
+
+    st.write(dfe)
 
 def upload_file():
     with st.container():
