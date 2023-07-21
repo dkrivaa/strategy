dbc738c22a07ea1ba0a04bcf5f3f601d11356e53
dkrivaa <dannykrivaa@hotmail.com>
2023-07-21 11:09:04 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -8,7 +8,7 @@ def home_title():
                        page_icon=url)
     st.image(url)
     st.markdown(f'<span style="color: #18448c; '
-                f'font-size: 32px"><b>StraApp - Strategic Planning</b></span>'
+                f'font-size: 24px"><b>StraApp - Strategic Planning</b></span>'
                 , unsafe_allow_html=True)
 
 
