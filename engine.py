91e94b9cea0ddb110ccc1b930895a54340e556e8
dkrivaa <dannykrivaa@hotmail.com>
2023-07-22 20:00:44 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -45,7 +45,7 @@ def data_input():
                 placeholder.empty()
                 sleep(0.01)
 
-                st.write(st.session_state.num)
+            st.write(st.session_state.num)
 
 
     # parameter = st.text_input(f'parameter {num +1}', key=num+1)
