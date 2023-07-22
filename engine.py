d9424b90b7bcec651db9e6871315dbe8fbd227ca
dkrivaa <dannykrivaa@hotmail.com>
2023-07-22 21:42:05 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -94,6 +94,77 @@ def data_external():
             return dfe
 
 
+def data_internal():
+    # This function enables the user to enter data -
+    # internal variables affecting the organization
+    with st.form('internal_form'):
+
+        parameter1 = st.text_input(f'**Parameter** #1', key='param1')
+        if parameter1 is None:
+            parameter1 = 0
+        significance1 = st.radio('**Significance** to organization',
+                                ['low', 'average', 'above average', 'high'],
+                                 index=3, horizontal=True, key='sig1')
+        probability1 = st.slider('**Probability**', min_value=0, max_value=100,
+                                 step=5, value=(45, 55), key='prob1')
+        st.markdown('___')
+
+        parameter2 = st.text_input(f'**Parameter** #2', key='param2')
+        if parameter2 is None:
+            parameter2 = 0
+        significance2 = st.radio('**Significance** to organization',
+                                 ['low', 'average', 'above average', 'high'],
+                                 index=3, horizontal=True, key='sig2')
+        probability2 = st.slider('**Probability**', min_value=0, max_value=100,
+                                 step=5, value=(45, 55), key='prob2')
+        st.markdown('___')
+
+        parameter3 = st.text_input(f'**Parameter** #3', key='param3')
+        if parameter3 is None:
+            parameter3 = 0
+        significance3 = st.radio('**Significance** to organization',
+                                 ['low', 'average', 'above average', 'high'],
+                                 index=3, horizontal=True, key='sig3')
+        probability3 = st.slider('**Probability**', min_value=0, max_value=100,
+                                 step=5, value=(45, 55), key='prob3')
+        st.markdown('___')
+
+        parameter4 = st.text_input(f'**Parameter** #4', key='param4')
+        if parameter4 is None:
+            parameter4 = 0
+        significance4 = st.radio('**Significance** to organization',
+                                 ['low', 'average', 'above average', 'high'],
+                                 index=3, horizontal=True, key='sig4')
+        probability4 = st.slider('**Probability**', min_value=0, max_value=100,
+                                 step=5, value=(45, 55), key='prob4')
+        st.markdown('___')
+
+        parameter5= st.text_input(f'**Parameter** #5', key='param5')
+        if parameter5 is None:
+            parameter5 = 0
+        significance5 = st.radio('**Significance** to organization',
+                                 ['low', 'average', 'above average', 'high'],
+                                 index=3, horizontal=True, key='sig5')
+        probability5 = st.slider('**Probability**', min_value=0, max_value=100,
+                                 step=5, value=(45, 55), key='prob5')
+        st.markdown('___')
+
+        submit_internal = st.form_submit_button(type='primary')
+
+        if submit_internal:
+            parameter = [parameter1, parameter2, parameter3, parameter4, parameter5]
+            significance = [significance1, significance2, significance3,
+                            significance4, significance5]
+            prob_low = [probability1[0], probability2[0], probability3[0],
+                        probability4[0], probability5[0]]
+            prob_high = [probability1[1], probability2[1], probability3[1],
+                         probability4[1], probability5[1]]
+            dfi = pd.DataFrame({'parameter': parameter,
+                                'significance': significance,
+                                'prob_low': prob_low,
+                                'prob_high': prob_high})
+            return dfi
+
 def upload_file():
     with st.container():
         user_file = st.file_uploader('Upload your file (.xlsx or .csv)',
---
+++ pages/02_Internal_Parameters.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+import engine
+
+engine.home_title()
+st.markdown(f'<span style="color: #ed7011; '
+            f'font-size: 18px"><b>'
+            f'Internal parameters affecting the organization'
+            f'</b></span>'
+            , unsafe_allow_html=True)
+st.markdown('___')
+
+st.markdown(f'<span style="color: #ed7011; '
+            f'font-size: 14px"><b>'
+            f'Enter up to 5 internal parameters'
+            f'</b></span>'
+            , unsafe_allow_html=True)
+
+dfi = engine.data_internal()
+
+st.write(dfi)
