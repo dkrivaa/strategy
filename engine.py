7e5ac12ec7c7115607e3848d5879417981c93813
dkrivaa <dannykrivaa@hotmail.com>
2023-07-22 12:49:49 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -24,61 +24,28 @@ def icon():
 def data_input():
     # This function enables the user to enter data -
     # external and internal variables affecting the organization
+    if 'num' not in st.session_state:
+        st.session_state.num = 0
+
     placeholder = st.empty()
-    e_form = placeholder.form('external_vars')
-    with e_form:
-        st.write('**EXTERNAL** parameters')
+    num = st.session_state.num
 
-        e_var1 = st.text_input('**#1**.')
-        e_sig1 = st.radio('**Significance to organization**',
-                          ['low', 'average', 'above average', 'high'],
-                          index=3, horizontal=True, key='evar1')
-        e_prob1 = st.slider('**probability**', min_value=0, max_value=100,
-                            step=5, value=(45, 55), key='s_evar1')
-        st.markdown('___')
+    with placeholder.form(key=str(num)):
+        st.write('test' + str(num))
 
-        e_var2 = st.text_input('**#2**.')
-        e_sig2 = st.radio('**Significance to organization**',
-                          ['low', 'average', 'above average', 'high'],
-                          index=3, horizontal=True, key='evar2')
-        e_prob2 = st.slider('**probability**', min_value=0, max_value=100,
-                            step=5, value=(45, 55), key='s_evar2')
-        st.markdown('___')
+        if st.form_submit_button():
+            st.session_state.num += 1
 
-        e_var3 = st.text_input('**#3**.')
-        e_sig3 = st.radio('**Significance to organization**',
-                          ['low', 'average', 'above average', 'high'],
-                          index=3, horizontal=True, key='evar3')
-        e_prob3 = st.slider('**probability**', min_value=0, max_value=100,
-                            step=5, value=(45, 55), key='s_evar3')
-        st.markdown('___')
 
-        e_var4 = st.text_input('**#4**.')
-        e_sig4 = st.radio('**Significance to organization**',
-                          ['low', 'average', 'above average', 'high'],
-                          index=3, horizontal=True, key='evar4')
-        e_prob4 = st.slider('**probability**', min_value=0, max_value=100,
-                            step=5, value=(45, 55), key='s_evar4')
-        st.markdown('___')
 
-        e_var5 = st.text_input('**#5**.')
-        e_sig5 = st.radio('**Significance to organization**',
-                          ['low', 'average', 'above average', 'high'],
-                          index=3, horizontal=True, key='evar5')
-        e_prob5 = st.slider('**probability**', min_value=0, max_value=100,
-                            step=5, value=(45, 55), key='s_evar5')
-        st.markdown('___')
-
-        st.form_submit_button('Continue', type='primary')
-
-    # e_var = []
-    # e_var_sig = []
-    # e_var_prob = []
-    # for i in range(0, 5):
-    #     e_var.append('e_var' + str(i))
-    #     e_var_sig.append(('e_var' + str(i) + '_sig'))
-    #     e_var_prob.append(('e_var' + str(i) + '_prob'))
-    # dfe = pd.DataFrame({'e_var': e_var, 'e_var_sig': e_var_sig, 'e_var_prob': e_var_prob})
+    # parameter = st.text_input(f'parameter {num +1}', key=num+1)
+    #         if parameter is None:
+    #             parameter = 0
+    #         significance = st.radio('Significance to organization',
+    #                                 ['low', 'average', 'above average', 'high'],
+    #                                 index=3, horizontal=True, key=num+10)
+    #         probability = st.slider('probability', min_value=0, max_value=100,
+    #                                 step=5, value=(45, 55), key=num+100)
 
 
 def upload_file():
@@ -103,51 +70,7 @@ def explanation():
         st.write('* promote explicit strategic decisions')
 
 
-def data_input1():
-    data = {'parameter': [], 'significance': [], 'probability_low': [],
-            'probability_high': []}
-    df = pd.DataFrame(data)
 
-    if 'df' not in st.session_state:
-        st.session_state.df = df
-
-
-#
-# def internal():
-
-    if 'num' not in st.session_state:
-        st.session_state.num = 0
-
-    st.write(st.session_state.num)
-
-    placeholder = st.empty()
-    num = st.session_state.num
-
-    with placeholder.form(key=str(num)):
-        parameter = st.text_input(f'parameter {num +1}', key=num+1)
-        if parameter is None:
-            parameter = 0
-        significance = st.radio('Significance to organization',
-                                ['low', 'average', 'above average', 'high'],
-                                index=3, horizontal=True, key=num+10)
-        probability = st.slider('probability', min_value=0, max_value=100,
-                                step=5, value=(45, 55), key=num+100)
-
-        if st.form_submit_button():
-            st.session_state.num += 1
-            new_row = {'parameter': parameter,
-                       'significance': significance,
-                       'probability_low': probability[0],
-                       'probability_high': probability[1]}
-            df.loc[len(df)] = new_row
-
-            if st.session_state.num >= 5:
-                placeholder.empty()
-        else:
-            st.stop()
-        st.write(st.session_state.df)
-
-        # st.experimental_rerun()
 
 
 
