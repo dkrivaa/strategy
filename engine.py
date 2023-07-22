dab1bb97850dc9a71ad15ec3ef09a09f8d715809
dkrivaa <dannykrivaa@hotmail.com>
2023-07-22 08:21:06 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -112,21 +112,22 @@ def data_input1():
     placeholder = st.empty()
     num = st.session_state.num
 
-    with placeholder.form(key=str(num)):
-        parameter = st.text_input(f'parameter {num +1}', key=num+1)
-        significance = st.radio('Significance to organization',
-                                ['low', 'average', 'above average', 'high'],
-                                index=3, horizontal=True, key=num+10)
-        probability = st.slider('probability', min_value=0, max_value=100,
-                                step=5, value=(45, 55), key=num+100)
+    for num in range(0, 5):
+        with placeholder.form(key=str(num)):
+            parameter = st.text_input(f'parameter {num +1}', key=num+1)
+            significance = st.radio('Significance to organization',
+                                    ['low', 'average', 'above average', 'high'],
+                                    index=3, horizontal=True, key=num+10)
+            probability = st.slider('probability', min_value=0, max_value=100,
+                                    step=5, value=(45, 55), key=num+100)
 
-        if st.form_submit_button():
-            st.session_state.num += 1
-            if st.session_state.num >= 3:
-                st.session_state.num = 0
-                placeholder.empty()
-        else:
-            st.stop()
+            if st.form_submit_button():
+                st.session_state.num += 1
+                if st.session_state.num >= 5:
+                    st.session_state.num = 0
+                    placeholder.empty()
+            else:
+                st.stop()
 
 
 
