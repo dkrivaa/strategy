7b12b58ddac7cc3f317a6992fe5b003dc09ad660
dkrivaa <dannykrivaa@hotmail.com>
2023-07-23 16:30:13 +0300

    Strategic Planning App

--- pages/04_Analyze.py
+++ pages/04_Analyze.py
@@ -12,3 +12,8 @@ st.markdown(f'<span style="color: #ed7011; '
             , unsafe_allow_html=True)
 st.markdown('___')
 
+url = 'https://cdn4.iconfinder.com/data/icons/success-filloutline/64/' \
+      'chart-analysis-analytics-data_analytics-pie-48.png'
+
+st.image(url)
+
