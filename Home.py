c821c0121ecf73d0fe1cca8dc4b6449d55bf4b46
dkrivaa <dannykrivaa@hotmail.com>
2023-07-23 14:55:47 +0300

    Strategic Planning App

--- Home.py
+++ Home.py
@@ -14,17 +14,8 @@ st.markdown(f'<span style="color: #ed7011; '
             , unsafe_allow_html=True)
 st.markdown('___')
 
+engine.home_menu()
 
-with st.container():
-    col1, col2, col3 = st.columns([1, 1, 8])
-    url1 = 'https://cdn1.iconfinder.com/data/icons/unicons-line-vol-3/24/edit-48.png'
-    url2 = 'https://cdn1.iconfinder.com/data/icons/unicons-line-vol-6/24/upload-48.png'
-    with col1:
-        st.image(url1)
-        st.button('Enter data')
-    with col2:
-        st.image(url2)
-        st.button('Upload data file')
 
 engine.upload_file()
 
--- engine.py
+++ engine.py
@@ -21,6 +21,20 @@ def icon():
     return url
 
 
+def home_menu():
+    with st.container():
+        col1, col2, col3 = st.columns([1, 1, 8])
+        url1 = 'https://cdn1.iconfinder.com/data/icons/unicons-line-vol-3/24/edit-48.png'
+        url2 = 'https://cdn1.iconfinder.com/data/icons/unicons-line-vol-6/24/upload-48.png'
+        with col1:
+            st.image(url1)
+            st.button('Enter data')
+        with col2:
+            st.image(url2)
+            st.button('Upload data file')
+
+
+
 def data_external():
     # This function enables the user to enter data -
     # external variables affecting the organization
