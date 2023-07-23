3602a35ea18ae8f00a0c21d82188fafe5d11458b
dkrivaa <dannykrivaa@hotmail.com>
2023-07-23 14:44:49 +0300

    Strategic Planning App

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ st.markdown('___')
 with st.container():
     col1, col2, col3 = st.columns([1, 1, 8])
     url1 = 'https://cdn1.iconfinder.com/data/icons/unicons-line-vol-3/24/edit-48.png'
-    url2 = 'https://cdn0.iconfinder.com/data/icons/phosphor-regular-vol-4/256/upload-simple-48.png'
+    url2 = 'https://cdn1.iconfinder.com/data/icons/unicons-line-vol-6/24/upload-48.png'
     with col1:
         st.image(url1)
         st.button('Enter data')
