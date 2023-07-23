2d34d9e8e0311a70307caab69c846274c59c3da5
dkrivaa <dannykrivaa@hotmail.com>
2023-07-23 08:16:10 +0300

    Strategic Planning App

--- Home.py
+++ Home.py
@@ -13,6 +13,9 @@ st.markdown(f'<span style="color: #ed7011; '
             , unsafe_allow_html=True)
 st.markdown('___')
 
-st.dataframe(engine.parameters())
+df = pd.concat([st.session_state['dfe'], st.session_state['dfi']], axis=0)
+st.write(df)
+
+
 engine.upload_file()
 
