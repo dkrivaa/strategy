e8ea796c41a1e2da960a09bdcdee1220bf86a6ff
dkrivaa <dannykrivaa@hotmail.com>
2023-07-23 20:46:15 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -358,7 +358,7 @@ def explanation():
 
 def edit_data():
     # Defining df - from uploaded file or from data entering
-    if st.session_state.user_file is not None:
+    if st.session_state.user_file:
         df = pd.read_csv(upload_file())
     else:
         df = parameters()
