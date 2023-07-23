e501a79ee2a6a24ecaad621ede02c678d1044b92
dkrivaa <dannykrivaa@hotmail.com>
2023-07-23 16:32:41 +0300

    Strategic Planning App

---
+++ pages/04_Edit_data.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+import engine
+
+# Setting top of page
+engine.home_title()
+
+st.markdown(f'<span style="color: #ed7011; '
+            f'font-size: 18px"><b>'
+            f'Edit your data'
+            f'</b></span>'
+            , unsafe_allow_html=True)
+st.markdown('___')
