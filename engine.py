91697f8b1b1e350bfd852bfce39706fc7fc0cff4
dkrivaa <dannykrivaa@hotmail.com>
2023-07-24 18:54:09 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -23,12 +23,16 @@ def icon():
 
 def home_menu():
     with st.container():
-        url1 = 'https://cdn4.iconfinder.com/data/icons/success-filloutline/64/' \
-               'goal-target-idea-think-objective-64.png'
-        st.image(url1)
-        st.write('1. Data')
-        st.write('2. Analyze')
-        st.write('3. Results')
+        st.button('**Data**')
+        st.button('**Analyze**')
+        st.button('**Results**')
+
+        # url1 = 'https://cdn4.iconfinder.com/data/icons/success-filloutline/64/' \
+        #        'goal-target-idea-think-objective-64.png'
+        # st.image(url1)
+        # st.write('1. Data')
+        # st.write('2. Analyze')
+        # st.write('3. Results')
     st.markdown('___')
 
     with st.container():
