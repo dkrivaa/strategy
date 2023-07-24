1153b4e3ed249bf6c78ad377a879c0b13b6fbd0d
dkrivaa <dannykrivaa@hotmail.com>
2023-07-24 16:34:52 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -404,8 +404,9 @@ def edit_data():
     if df.shape == (0, 0):
         st.warning('There is no data. Please enter data on relevant pages')
 
-    st.data_editor(data=df,
-                   )
+    st.data_editor(data=df, column_config={'significance':
+                                           st.column_config.SelectboxColumn('significance',
+                                                options=['low', 'average', 'above average', 'high'])})
 
 
 
