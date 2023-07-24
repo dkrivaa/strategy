683766a54aa4a1806aa4fc83d95c55aeeaf80127
dkrivaa <dannykrivaa@hotmail.com>
2023-07-24 08:56:45 +0300

    Strategic Planning App

--- pages/testpage.py
+++ pages/testpage.py
@@ -3,6 +3,11 @@ import streamlit as st
 
 my_file = st.file_uploader('Upload your **parameters** file (.csv)',type=['csv'])
 
+if 'my_file' not in st.session_state:
+    st.session_state.my_file = my_file
+
+st.write(st.session_state)
+
 df = pd.read_csv(my_file)
 
 st.write(df)
