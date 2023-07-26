86769e7569701a15e11b0e993f4b6293d502db65
dkrivaa <dannykrivaa@hotmail.com>
2023-07-26 11:30:50 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -432,7 +432,7 @@ def analyze():
 
     for i in range(len(df)):
         my_list = ['average', 'above average', 'high', 'very high']
-        df['sig_code'] = my_list.index(df['significance'][i])
+        df['sig_code'] = my_list.index(df['significance'][i]) + 1
         st.write(df['sig_code'][i])
 
 
