7a80170ed696210b1f337a2e289d2c36da6a1635
dkrivaa <dannykrivaa@hotmail.com>
2023-07-27 11:13:28 +0300

    Strategic Planning App

--- engine.py
+++ engine.py
@@ -440,8 +440,8 @@ def analyze():
 
     c = altair.Chart(dfa).mark_point().encode(
         x='prob_high',
-        y=dfa['sig_code'],
-        size=dfa['sig_code']
+        y='sig_code',
+        size='sig_code'
         )
     st.altair_chart(c, use_container_width=True)
 
